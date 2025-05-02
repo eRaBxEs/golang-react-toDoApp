1f85993a8cfe83a4b14c24f4b80ff85366cb8742
Ehioje Henry Erabor <ehiojee@yahoo.com>
2025-05-02 23:58:12 +0100

    create the layout for all the routers in the middleware

---
+++ server/middleware/middleware.go
@@ -0,0 +1,35 @@
+package middleware
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+	"os"
+)
+
+func GetAllTasks(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func CreateTask(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func TaskComplete(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func UndoTask(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func DeleteTask(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func DeleteAllTasks(w http.ResponseWriter, r *http.Request) {
+
+}
