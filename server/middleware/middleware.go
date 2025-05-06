e6866ff527785f991d3275faaee90b329f5499ec
Ehioje Henry Erabor <ehiojee@yahoo.com>
2025-05-06 13:53:15 +0100

    add functions for loading environmental variables and for database instance

--- server/middleware/middleware.go
+++ server/middleware/middleware.go
@@ -4,11 +4,52 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
+func init() {
+	loadTheEnv()
+	createDBInstance()
+}
+
+func loadTheEnv() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("error loading the .env")
+	}
+}
+
+func createDBInstance() {
+	connectionString := os.Getenv("DB_URI")
+	dbName := os.Getenv("DB_NAME")
+	collName := os.Getenv("DB_COLLECTION_NAME")
+
+	clientOptions := options.Client().ApplyURL(connectionString)
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("connected to mongodb")
+
+	collection := client.Database(dbName).Collection(collName)
+	fmt.Println("collection instance created")
+
+}
 
 func GetAllTasks(w http.ResponseWriter, r *http.Request) {
 
