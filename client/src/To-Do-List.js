bbfc849af0589e5b9356b12b6c11bdda54f4b520
Ehioje Henry Erabor <ehiojee@yahoo.com>
2025-05-08 15:22:20 +0100

    working on the getTask frontend code

--- client/src/To-Do-List.js
+++ client/src/To-Do-List.js
@@ -18,6 +18,72 @@ class ToDoList extends Component {
         this.getTask();
     }
 
+    onChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    };
+
+    onSubmit
+
+    getTask = () => {
+        axios.get(endpoint + "/api/task").then((res) => {
+            if (res.data) {
+                this.setState({
+                    items: res.data.map((item) => {
+                        let color = "yellow";
+                        let style = {
+                            wordWrap: "break-word",
+                        };
+
+                        if (item.status) {
+                            color = "green";
+                            style["textDecoration"] = "line-through";
+                        }
+
+                        return (
+                            <Card key={item._id} color={color} fluid className="rough">
+                                <Card.Content>
+                                    <Card.Header textAlign="left">
+                                        <div style={style}>{item.task}</div>
+                                    </Card.Header>
+
+                                    <Card.Meta textAlign="right">
+                                        <Icon
+                                            name="check circle"
+                                            color="blue"
+                                            onClick={() => this.updateTask(item._id)}
+                                        />
+
+                                        <span style={{ paddingRight: 10 }}>Undo</span>
+
+                                        <Icon
+                                            name="delete"
+                                            color="red"
+                                            onClick={() => this.deleteTask(item._id)}
+                                        />
+                                        <span style={{ paddingRight: 10 }}>Delete</span>
+                                    </Card.Meta>
+                                </Card.Content>
+                            </Card >
+                        );
+
+                    }),
+                });
+            } else {
+                this.setState({
+                    items: [],
+                });
+            }
+        });
+    };
+
+    updateTask
+
+    undoTask
+
+    deleteTask
+
 
     render() {
         return (
@@ -39,8 +105,11 @@ class ToDoList extends Component {
 
                         />
 
-
+                        {/*<Button>Create Task</Button>*/}
                     </Form>
+                </div>
+                <div className="row">
+                    <Card.Group>{this.state.items}</Card.Group>
                 </div>
             </div>
         );
