188f3d88eee9d381a3d6b1cb976a75b746f94e3c
Ehioje Henry Erabor <ehiojee@yahoo.com>
2025-05-10 06:19:13 +0100

    add cors features

--- server/main.go
+++ server/main.go
@@ -6,11 +6,21 @@ import (
 	"net/http"
 
 	"github.com/erabxes/golang-react-todo/router"
+	"github.com/rs/cors"
 )
 
 func main() {
 	r := router.Router()
-	fmt.Println("Starting server on port:9000")
 
-	log.Fatal(http.ListenAndServe(":9000", r))
+	// Add CORS middleware
+	c := cors.New(cors.Options{
+		AllowedOrigins: []string{"http://localhost:3000"}, // Your React port
+		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders: []string{"*"},
+	})
+
+	handler := c.Handler(r)
+
+	fmt.Println("Starting server on port:9000")
+	log.Fatal(http.ListenAndServe(":9000", handler))
 }
